a7c2363f13e7c29d64c4023afd8ab9ad32cabaf5
IDEFIX95 <104372704+IDEFIX95@users.noreply.github.com>
2023-10-04 16:11:47 +0200

    Update exo2.c

--- TD_N°2/exo2.c
+++ TD_N°2/exo2.c
@@ -4,18 +4,46 @@
 typedef struct piledyn{
     int element;
     struct piledyn * suivant;
-}PileDyn;
+}Piledyn;
 
 
-PileDyn * creationPile(int valeur){
-    PileDyn * nouveau = malloc(sizeof(PileDyn));
+Piledyn * empiler(Piledyn * pile,int valeur){
+    Piledyn * nouveau = malloc(sizeof(Piledyn));
     if(nouveau == NULL){
+        printf("erreur");
         exit(1);
     }
     nouveau -> element = valeur;
+    nouveau -> suivant = pile;
+    pile = nouveau;
+    return pile;
 }
 
-PileDyn * empiler(PileDyn * ppile, int valeur){
-    PileDyn *nouveau = 
-    
+void affichePile(Piledyn * pile){
+    Piledyn * p = pile;
+    while(pile != NULL){
+        printf("%d", p ->suivant);
+        p -> suivant;
+    }
+}
+
+void affichePileRecursif(Piledyn * pile){
+    if(pile != NULL){
+        printf("%d", pile -> element);
+        affichePileRecursif(pile -> suivant);
+    }
+}
+
+
+
+Piledyn * depile(Piledyn * pile, int * pnmb){
+    Piledyn * p = pile;
+    if(p == NULL){
+        printf("impossible de depiler");
+        exit(1);
+    }
+    *pnmb = p -> element;
+    pile = p -> suivant;
+    free(p);
+    return pile;
 }
