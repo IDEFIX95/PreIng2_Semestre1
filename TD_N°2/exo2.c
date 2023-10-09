00e187c8d5146749c4787b16c0d46384e45e7f02
IDEFIX95 <104372704+IDEFIX95@users.noreply.github.com>
2023-10-09 17:58:12 +0200

    Add files via upload

--- TD_N°2/exo2.c
+++ TD_N°2/exo2.c
@@ -1,35 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+/* Question 1 */
+
 typedef struct piledyn{
     int element;
     struct piledyn * suivant;
 }Piledyn;
 
 
-Piledyn * empiler(Piledyn * pile,int valeur){
+/* Question 2 */
+
+Piledyn * empiler(Piledyn * ppile,int nb){
     Piledyn * nouveau = malloc(sizeof(Piledyn));
     if(nouveau == NULL){
         printf("erreur");
         exit(1);
     }
-    nouveau -> element = valeur;
-    nouveau -> suivant = pile;
-    pile = nouveau;
-    return pile;
+    nouveau -> element = nb;
+    nouveau -> suivant = ppile;
+    ppile = nouveau;
+    return ppile;
 }
 
 void affichePile(Piledyn * pile){
     Piledyn * p = pile;
     while(pile != NULL){
-        printf("%d", p ->suivant);
+        printf("%d\n", p ->element);
         p -> suivant;
     }
 }
 
+
 void affichePileRecursif(Piledyn * pile){
     if(pile != NULL){
-        printf("%d", pile -> element);
+        printf("%d\n", pile -> element);
         affichePileRecursif(pile -> suivant);
     }
 }
@@ -46,4 +52,53 @@ Piledyn * depile(Piledyn * pile, int * pnmb){
     pile = p -> suivant;
     free(p);
     return pile;
+}
+
+Piledyn * pilepair(Piledyn * pile){
+    Piledyn * p1 = pile;
+    Piledyn * pile_pair = NULL;
+    while(p1 != NULL)
+    {
+        if(p1->element % 2 == 0){
+          pile_pair = empiler(pile_pair, p1->element);
+        }
+        p1=p1->suivant;
+    }
+    
+    return pile_pair;
+}
+
+
+Piledyn * pileimpair(Piledyn * pile){
+
+}
+
+int main(){
+    /* Suite Question 2 */
+    Piledyn * nouvelle_pile = NULL;
+    Piledyn * p1 = nouvelle_pile;
+    Piledyn * nouvelle_pile_pair = NULL;
+    Piledyn * nouvelle_pile_impair = NULL;
+    for(int i=1;i<21;i++){
+        nouvelle_pile = empiler(nouvelle_pile,i);
+    }
+    affichePile(nouvelle_pile);
+    
+
+    while(depile(nouvelle_pile) != NULL){
+        while(p1 != NULL)
+        {
+            if(p1->element % 2 == 0){
+                nouvelle_pile_pair = empiler(nouvelle_pile_pair, p1->element);
+            }
+            else{
+                nouvelle_pile_impair = empiler(nouvelle_pile_impair, p1->element);
+            }
+        p1=p1->suivant;
+        }
+    }
+    affichePile(nouvelle_pile_pair);
+
+
+    return 0;
 }
