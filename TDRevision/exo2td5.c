42b3cc2b35021b532ee896d3e8bbcf82ba3f546d
IDEFIX95 <104372704+IDEFIX95@users.noreply.github.com>
2023-11-22 00:39:21 +0100

    Update exo2td5.c

--- TDRevision/exo2td5.c
+++ TDRevision/exo2td5.c
@@ -35,7 +35,7 @@ int recherche(parbre a, int e){
 
 parbre insertABR(parbre a, int e){
     if(a == NULL){
-        return 1;
+        exit(1);
     }
     if(a->element > e){
         a->fg = insertABR(a->fg, e);
@@ -48,7 +48,7 @@ parbre insertABR(parbre a, int e){
 
 parbre insertABR2(parbre a, int e){
     if(a == NULL){
-        return 1;
+        exit(1);
     }
     parbre nouveau = creerarbre(e);
     if(a->element > nouveau -> element){
@@ -60,7 +60,7 @@ parbre insertABR2(parbre a, int e){
     return a;
 }
 
-parbre supp_noeud(parbre a, int e){
+/* parbre supp_noeud(parbre a, int e){
     parbre tmp;
     if(a == NULL){
         return a;
@@ -96,14 +96,14 @@ parbre supp_Max(parbre a, int * e){
         free(p1);
     }
     return a;
-}
+} */
 
 
 int existeFilsGauche(parbre a){
     if(a == NULL){
         return 1;
     }
-    else if(a->fg != NULL){
+    else if(a->fg == NULL){
         return 0;
     }
     else{
@@ -111,7 +111,79 @@ int existeFilsGauche(parbre a){
     }
 }
 
+int existeFilsDroit(parbre a){
+    if(a == NULL){
+        return 1;
+    }
+    else if(a->fd == NULL){
+        return 0;
+    }
+    else{
+        return 1;
+    }
+}
+
+parbre ajouterFilsGauche(parbre a, int valeur){
+    parbre nouveau = creerarbre(valeur);
+    if(a == NULL){
+        exit(1);
+    }
+    else if(!existeFilsGauche(a)){
+        a->fg = nouveau;
+    }
+    return a;
+}
+
+parbre ajouterFilsDroit(parbre a, int valeur){
+    parbre nouveau = creerarbre(valeur);
+    if(a == NULL){
+        exit(1);
+    }
+    else if(!existeFilsDroit(a)){
+        a->fd = nouveau;
+    }
+    return a;
+}
+
+void traiter(parbre arbre)        // Question 2) a)
+{
+    printf("%d ",arbre->element);
+}
+
+int estVide(parbre a){              // Question 1) d)
+    if(a == NULL){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+void parcourPrefixe(parbre arbre){          // Question 2) b)
+    if(!estVide(arbre)){
+        traiter(arbre);
+        parcourPrefixe(arbre->fg);
+        parcourPrefixe(arbre->fd);
+    }
+}
 
 int main(){
+    parbre nouveau_arbre = creerarbre(1);       // Question 1) i)
+    parbre noeud_suivant = nouveau_arbre;
+    ajouterFilsDroit(nouveau_arbre,8);
+    ajouterFilsGauche(nouveau_arbre,2);
+    noeud_suivant = nouveau_arbre -> fg;
+    ajouterFilsGauche(noeud_suivant,3);
+    ajouterFilsDroit(noeud_suivant,6);
+    noeud_suivant = noeud_suivant -> fg;
+    ajouterFilsGauche(noeud_suivant,4);
+    ajouterFilsDroit(noeud_suivant,5);
+    noeud_suivant = nouveau_arbre -> fg;
+    noeud_suivant = noeud_suivant -> fd;
+    ajouterFilsDroit(noeud_suivant,7);
+    noeud_suivant = nouveau_arbre -> fd;
+    ajouterFilsDroit(noeud_suivant,10);
+    ajouterFilsGauche(noeud_suivant,9);
+    parcourPrefixe(nouveau_arbre);
     return 0;
 }
